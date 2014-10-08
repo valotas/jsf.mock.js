691c0c65e247dd0f0aceea372746385f5098151f
Georgios Valotasios <valotas@gmail.com>
2014-10-08 21:17:30 +0200

    Added task to lint files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/*jshint node:true*/
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+gulp.task('lint', function () {
+  return gulp.src(['gulpfile.js', '.jshintrc', 'src/**.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
+});
