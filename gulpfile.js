f71b2e2d40d158bb7489af5325103187b24955e8
Georgios Valotasios <valotas@gmail.com>
2014-10-11 13:15:01 +0200

    Automated the release process

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var stylish = require('jshint-stylish');
 var bump = require('gulp-bump');
 var argv = require('yargs').argv;
 var karma = require('karma').server;
+var git = require('gulp-git');
 
 gulp.task('lint', function () {
   return gulp.src(['gulpfile.js', '.jshintrc', 'src/**.js'])
@@ -39,4 +40,23 @@ gulp.task('bump', function () {
     .pipe(gulp.dest('./'));
 });
 
+gulp.task('tag', function () {
+  var pkg = require('./package.json'),
+    v = 'v' + pkg.version,
+    msg = 'Release ' + v;
+
+  gulp.src('./')
+    .pipe(git.commit(msg))
+    .pipe(git.tag(v, msg))
+    .pipe(git.push('origin', 'master', '--tags'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('npm-publish', function (done) {
+  require('child_process').spawn('npm', ['publish'], { stdio: 'inherit' })
+    .on('close', done);
+});
+
 gulp.task('default', ['lint', 'ci-test']);
+
+gulp.task('release', ['bump', 'tag', 'npm-publish']);
